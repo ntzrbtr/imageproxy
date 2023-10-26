b125d635cfc3f3ef6caaff9b1486481bca8bfa53
Thomas Off <thomas.off@netzarbeiter.info>
2023-10-27 00:50:32 +0200

    Remove auth and event stuff as we don't use it.

--- config/app.php
+++ config/app.php
@@ -164,8 +164,6 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\GlideServiceProvider::class,
     ])->toArray(),
